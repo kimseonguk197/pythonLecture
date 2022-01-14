be14d4061d7b072a14afe754fdab75f9f4d1352f
kimseonguk <ksg3941234@gmail.com>
2022-01-14 21:59:49 +0900

    second commit

---
+++ python5/5-1.py
@@ -0,0 +1,15 @@
+#제곱 문제풀이 56p. 연습문제2.   x**y : x의 y제곱승
+# x=2
+# y = 2.5*(x**2) + 3.3*x + 6
+# print("2차 방정식 결과 = ", y)
+
+#리스트 연습문제1  110p.
+# lst = [10, 1, 5,2]
+# result = lst * 2
+# print("단계 1", result)
+# result.append(lst[0]*2)
+# print("단계2 :", result)
+# result2 = result[1:len(result):2]
+# # result2 = result[1::2]
+# print("단계3 :", result2)
+
---
+++ python5/5-2.py
@@ -0,0 +1,173 @@
+#while문의 기본구조는 while 조건문: 실행문 의 구조로서 while안의 조건문이 true인한 계속 반복된다.
+# number = 10
+# while number == 10:
+#     print("숫자는 10입니다.")
+
+#기본 while문 예제
+# treeHits = 0
+#
+# while treeHits < 10:
+#
+#     treeHits += 1 #뒤와 같이 표현도 가능. treeHits = treeHits +1
+#     print("나무를 %s번 찍었습니다" % treeHits)
+#     if treeHits == 10:
+#         print("나무가 넘어갑니다.")
+
+# #리스트 연습문제2 A형  110p.
+# listNum = int(input("리스트의 크기를 입력하세요"))
+# a = 0
+# lst =[]
+# while a < listNum:
+#     listNum1 = int(input("임의이 값을 입력하세요"))
+#     lst.append(listNum1)
+#     a += 1
+#
+# print(lst)
+
+# #리스트 연습문제2 A형 또다른 풀이.
+# import random
+# listNum = int(input("리스트의 크기를 입력하세요"))
+# a = 0
+# lst =[]
+# while a < listNum:
+#     lst.append(random.randint(1, 10))
+#     a += 1
+#
+# print(lst)
+
+# # #리스트 연습문제2 B형  110p.
+# listNum = int(input("리스트의 크기를 입력하세요"))
+# a = 0
+# lst =[]
+# while a < listNum:
+#     listNum1 = int(input("임의이 값을 입력하세요"))
+#     lst.append(listNum1)
+#     a += 1
+# if int(input("찾을 값을 입력하시오")) in lst:
+#     print("yes")
+# else:
+#     print("no")
+
+#while문 break문 예제
+# treeHits = 0
+# while True:
+#     treeHits += 1 #뒤와 같이 표현도 가능. treeHits = treeHits +1
+#     print("나무를 %s번 찍었습니다" % treeHits)
+#     if treeHits == 10:
+#         print("나무가 넘어갑니다.")
+#         break
+
+#연습문제 3장 문제2.
+# import random
+#
+# print("숫자 맞추기 게임")
+# com = random.randint(1, 10)
+# while True:
+#     my = int (input('예상 숫자를 입력 하세요'))
+#     if com == my:
+#         print('성공하셨습니다.')
+#         break
+#     elif com > my:
+#         print('더큰 숫자를 입력하세요')
+#     elif com < my:
+#         print('더 작은 숫자를 입력하세요')
+
+
+#while문 continue문 홀수만 출력 예제
+#
+# number = 0
+# while number < 100:
+#     number = number +1
+#     if number % 2 ==0:
+#         continue
+#     print(number)
+
+#while문 continue문 나무찍기 예제
+# treeHits = 1
+# while True:
+#     if treeHits <= 10:
+#         print("나무를 %s번 찍었습니다" % treeHits)
+#         treeHits += 1  # 뒤와 같이 표현도 가능. treeHits = treeHits +1
+#         continue
+#     print("나무가 넘어갑니다.")
+#     break
+
+
+# #for문 기본 예제1
+# lista = ['one', 'two', 'three']
+#
+# for a in lista:
+#     print(a)
+
+
+##for문 예제2
+# lista = [90, 25, 67, 45, 80]
+# number = 0
+# for a in lista:
+#     number +=1
+#     if a>=60:
+#         print("%s 번째 학생은 합격입니다." % number)
+#     else:
+#         print("%s 번째 학생은 불합격입니다." % number)
+
+
+
+#튜플로 구성된 리스트
+
+# tupleList = [(1,2), (3,4), (5,6)]
+# for (a,b) in tupleList:
+#     print(a + b)
+
+
+##for문과 continue
+# lista = [90, 25, 67, 45, 80]
+# number = 0
+# for a in lista:
+#     number += 1
+#     if a < 60:
+#         print("%s 번째 학생은 불합격입니다." % number)
+#         continue
+#     print("%s 번째 학생은 합격입니다." % number)
+
+# ##for문과 break >> a형을 찾는다. >> 선착순 1명만 찾는다고 한다고 할때
+# lista = ['b', 'b', 'ab', 'a', 'b', 'b']
+# number = 0
+# for a in lista:
+#     number += 1
+#     if a == 'a':
+#         print("%s 번째 고객이 이벤트에 당첨되었습니다." % number)
+#         break
+
+# #for문 range 예제
+# sum =0
+# for i in range(1, 101):
+#     sum = sum +i
+# print(sum)
+
+
+# ##for문 예제3 연습문제3
+# result = 0
+# print('수열 = ', end=" ")
+# for i in range(1,101):
+#     if ((i%3 ==0) and ((i%2) !=0)) :
+#         print(i, end=" ")
+#         result += i
+# print("누적합 = ", 867)
+
+#2중 for문 구구단 예제
+
+# for left in range(2,10):
+#     for right in range(1,10):
+#         result = left*right
+#         print("%d x %d = %d" %(left, right, result))
+
+#2중 for문 리스트에 담기
+
+# lista = []
+# for left in range(2,10):
+#     for right in range(1,10):
+#         result = left*right
+#         lista.append(result)
+# print(lista)
+
+#112p. 연습문제 4번.
